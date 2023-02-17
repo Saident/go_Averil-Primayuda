ddcbaacf9017e3650fc7fbd22454031ccf238e0c
Saident <averilprimayuda@gmail.com>
2023-02-17 17:47:17 +0700

    prioritas 1-4

--- 5_Basic Programming/Praktikum/prioritas1-4/prioritas1-4.go
+++ 5_Basic Programming/Praktikum/prioritas1-4/prioritas1-4.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Scan()
+	fmt.Println("Angka 1 - 100: ")
+
+	for i := 1; i < 101; i++ {
+		if i%3 == 0 {
+			fmt.Print(" Fizz")
+		} else if i%5 == 0 {
+			fmt.Print(" Buzz")
+		} else {
+			fmt.Print(" ", i, " ")
+		}
+	}
 }
