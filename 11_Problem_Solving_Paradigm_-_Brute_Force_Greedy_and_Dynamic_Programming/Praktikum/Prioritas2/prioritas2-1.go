8bcd5d9cb308c1865016e1dc6ab2fa48c0b31ba2
Saident <averilprimayuda@gmail.com>
2023-03-05 12:45:21 +0700

    prioritas 2-1 section 11 //uncomplete

--- 11_Problem_Solving_Paradigm_-_Brute_Force_Greedy_and_Dynamic_Programming/Praktikum/Prioritas2/prioritas2-1.go
+++ 11_Problem_Solving_Paradigm_-_Brute_Force_Greedy_and_Dynamic_Programming/Praktikum/Prioritas2/prioritas2-1.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func Frog(jumps []int) int {
+	for i := 0; i < len(jumps); i++ {
+
+	}
 	return 0
 }
 
-func main()  {
+func main() {
 	fmt.Println(Frog([]int{10, 30, 40, 20}))
 	fmt.Println(Frog([]int{30, 10, 60, 10, 60, 50}))
 }
