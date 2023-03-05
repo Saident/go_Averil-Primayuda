f191ca00678f14dc143a2abbd186d58be214f02d
Saident <averilprimayuda@gmail.com>
2023-03-05 11:11:49 +0700

    prioritas1-2.go

---
+++ 11_Problem_Solving_Paradigm_-_Brute_Force_Greedy_and_Dynamic_Programming/Praktikum/Prioritas1/prioritas1-2.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func toPascal(numRows int) [][]int {
+	output := make([][]int, numRows)
+
+	for i := 0; i < numRows; i++ {
+		arr := make([]int, i+1)
+		arr[0] = 1
+		arr[len(arr)-1] = 1
+		output[i] = arr
+		for j := 0; j < len(arr); j++ {
+			if i > 1 {
+				if j == 0 || j == len(arr)-1 {
+					arr[j] = 1
+				} else {
+					arr[j] = output[i-1][j-1] + output[i-1][j]
+				}
+			}
+		}
+		output[i] = arr
+	}
+	return output
+}
+
+func main() {
+	fmt.Println(toPascal(1))
+	fmt.Println(toPascal(2))
+	fmt.Println(toPascal(3))
+	fmt.Println(toPascal(5))
+}
