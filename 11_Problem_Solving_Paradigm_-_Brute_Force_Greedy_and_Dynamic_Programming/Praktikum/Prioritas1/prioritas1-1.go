9be2ece0997a0c5c6843484635bcbd6a57a98de0
Saident <averilprimayuda@gmail.com>
2023-03-05 12:45:02 +0700

    update prioritas1-1 section 11

--- 11_Problem_Solving_Paradigm_-_Brute_Force_Greedy_and_Dynamic_Programming/Praktikum/Prioritas1/prioritas1-1.go
+++ 11_Problem_Solving_Paradigm_-_Brute_Force_Greedy_and_Dynamic_Programming/Praktikum/Prioritas1/prioritas1-1.go
@@ -5,30 +5,13 @@ import (
 	"strconv"
 )
 
-func ans(number int) []string {
+func ans(number int64) []string {
 	ans := make([]string, number+1)
-	for i := 0; i <= number; i++ {
-		ans[i] = setBit(i)
+	var i int64
+	for i = 0; i <= number; i++ {
+		ans[i] = strconv.FormatInt(i, 2)
 	}
 	return ans
-}
-
-func setBit(number int) string {
-	out := ""
-	if number == 0 {
-		return "0"
-	} else if number == 1 {
-		return "1"
-	} else {
-		for number > 0 {
-			for number > 0 {
-				ext := number & 1
-				out = strconv.Itoa(ext) + out
-				number >>= 1
-			}
-		}
-		return out
-	}
 }
 
 func main() {
