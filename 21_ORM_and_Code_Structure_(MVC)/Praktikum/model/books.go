d380c1f2b7ed6f002870a4630a3a00649e4240b0
Saident <averilprimayuda@gmail.com>
2023-04-05 05:00:35 +0700

    Model Books

---
+++ 21_ORM_and_Code_Structure_(MVC)/Praktikum/model/books.go
@@ -0,0 +1,12 @@
+package model
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type Book struct {
+	gorm.Model
+	Judul    string `json:"judul" form:"judul"`
+	Penulis  string `json:"penulis" form:"penulis"`
+	Penerbit string `json:"penerbit" form:"penerbit"`
+}
