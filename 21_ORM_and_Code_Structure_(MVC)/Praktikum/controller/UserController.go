e88da728711d0c9a6994e57c522ff84aae7f61fe
Saident <averilprimayuda@gmail.com>
2023-04-05 05:00:23 +0700

    UserController

---
+++ 21_ORM_and_Code_Structure_(MVC)/Praktikum/controller/UserController.go
@@ -0,0 +1,102 @@
+package controller
+
+import (
+	"net/http"
+	"strconv"
+
+	"praktikum_21/config"
+	"praktikum_21/model"
+
+	"github.com/labstack/echo"
+)
+
+// get all users
+func GetUsersController(c echo.Context) error {
+	var users []model.User
+
+	if err := config.DB.Find(&users).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success get all users",
+		"users":   users,
+	})
+}
+
+// get user by id
+func GetUserController(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		echo.NewHTTPError(http.StatusBadRequest, "messages: invalid id parameter")
+	}
+
+	var users model.User
+	if err := config.DB.First(&users, id).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success get user by id",
+		"user":    users,
+	})
+}
+
+// create new user
+func CreateUserController(c echo.Context) error {
+	user := model.User{}
+	c.Bind(&user)
+
+	if err := config.DB.Save(&user).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success create new user",
+		"user":    user,
+	})
+}
+
+// delete user by id
+func DeleteUserController(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		echo.NewHTTPError(http.StatusBadRequest, "messages: invalid id parameter")
+	}
+
+	var user model.User
+	if err := config.DB.First(&user, id).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	if err := config.DB.Delete(&user).Error; err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success delete user by id",
+	})
+}
+
+// update user by id
+func UpdateUserController(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		echo.NewHTTPError(http.StatusBadRequest, "messages: invalid id parameter")
+	}
+
+	var user model.User
+
+	if err := config.DB.First(&user, id).Error; err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, err.Error())
+	}
+
+	c.Bind(&user)
+
+	if err := config.DB.Save(&user).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success update user by id",
+		"user":    user,
+	})
+}
